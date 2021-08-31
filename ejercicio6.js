93c1a695aecee0d0a0e545cfee43af14056ef976
unknown <sv73107535@idat.edu.pe>
2021-08-31 01:34:50 -0500

    first commit

--- ejercicio6.js
+++ ejercicio6.js
@@ -1,8 +1,7 @@
-let sche = {}
-
 let schedule = {};
-
+schedule["8:30"] = "get up"
 function isEmpty(obj) {
-    this.obj= obj === {}
+    this.obj= obj[""] === schedule["8:30"]
 }
+
 console.log(new isEmpty(schedule))
